bf010fad9ffd5302f102bf9c9bfb12d7acfdadb9
codexu <461229187@qq.com>
2025-09-02 15:09:32 +0800

    docs: update footer license from MIT to GPL-3.0 with link to license file

--- app/[lang]/(home)/footer.tsx
+++ app/[lang]/(home)/footer.tsx
@@ -1,3 +1,4 @@
+import Link from "next/link";
 import SectionWrap from "./section-wrap";
 import Image from "next/image";
 
@@ -8,7 +9,7 @@ export default function HomeFooter() {
         <Image src="https://www.netlify.com/assets/badges/netlify-badge-color-accent.svg" width={100} height={24} alt="Deploys by Netlify" />
       </a>
       <div className="flex flex-col gap-2 text-sm text-fd-muted-foreground">
-        <p>Released under the MIT License.</p>
+        <p>Released under the <Link href="https://github.com/codexu/note-gen/blob/dev/LICENSE">GPL-3.0 License</Link>.</p>
         <p>Copyright © 2024-present codexu</p>
       </div>
     </SectionWrap>
