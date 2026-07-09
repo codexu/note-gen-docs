7dbb3c3231d2b9f66d3157eb1fe673719bbef4f2
codexu <461229187@qq.com>
2026-07-09 09:28:13 +0800

    feat(deploy): 优化部署流程，更新打包和上传逻辑，使用预构建的独立包

--- ecosystem.config.js
+++ ecosystem.config.js
@@ -1,8 +1,7 @@
 module.exports = {
   apps: [{
     name: 'note-gen-docs',
-    script: 'node_modules/next/dist/bin/next',
-    args: 'start -p 8080',
+    script: 'server.js',
     cwd: '/root/note-gen-docs/current',
     exec_mode: 'cluster',
     instances: 2,
@@ -11,7 +10,6 @@ module.exports = {
       PORT: 8080,
       NODE_OPTIONS: '--max-old-space-size=2048'
     },
-    instances: 1,
     autorestart: true,
     watch: false,
     max_memory_restart: '2G'
--- next.config.mjs
+++ next.config.mjs
@@ -4,6 +4,7 @@ const withMDX = createMDX({});
 /** @type {import('next').NextConfig} */
 const config = {
   reactStrictMode: true,
+  output: 'standalone',
   images: {
     domains: ["s2.loli.net", "avatars.githubusercontent.com", "www.netlify.com"],
   },
