3bf518be0301029ef99af06530a8db0dd09f39b5
codexu <461229187@qq.com>
2026-07-13 09:49:12 +0800

    feat: load latest download version from CDN

--- src/config/downloads.ts
+++ src/config/downloads.ts
@@ -1,18 +1,22 @@
-export const LATEST_VERSION = '0.30.1';
+export const FALLBACK_VERSION = '0.30.1';
 
 const CDN_BASE_URL = 'https://download.notegen.top';
-const releasePath = `${CDN_BASE_URL}/releases/note-gen-v${LATEST_VERSION}`;
 
 export const GITHUB_RELEASES_URL = 'https://github.com/codexu/note-gen/releases/latest';
 
-export const downloadUrls = {
-  windows: `${releasePath}/NoteGen_${LATEST_VERSION}_x64-setup.exe`,
-  macosAppleSilicon: `${releasePath}/NoteGen_${LATEST_VERSION}_aarch64.dmg`,
-  macosIntel: `${releasePath}/NoteGen_${LATEST_VERSION}_x64.dmg`,
-  linuxAppImage: `${releasePath}/NoteGen_${LATEST_VERSION}_amd64.AppImage`,
-  linuxDeb: `${releasePath}/NoteGen_${LATEST_VERSION}_amd64.deb`,
-  linuxRpm: `${releasePath}/NoteGen-${LATEST_VERSION}-1.x86_64.rpm`,
-  androidApk: `${releasePath}/NoteGen_${LATEST_VERSION}_android-universal.apk`,
-} as const;
+export function getDownloadUrls(version: string) {
+  const releasePath = `${CDN_BASE_URL}/releases/note-gen-v${version}`;
 
-export type DownloadUrlKey = keyof typeof downloadUrls;
+  return {
+    windows: `${releasePath}/NoteGen_${version}_x64-setup.exe`,
+    macosAppleSilicon: `${releasePath}/NoteGen_${version}_aarch64.dmg`,
+    macosIntel: `${releasePath}/NoteGen_${version}_x64.dmg`,
+    linuxAppImage: `${releasePath}/NoteGen_${version}_amd64.AppImage`,
+    linuxDeb: `${releasePath}/NoteGen_${version}_amd64.deb`,
+    linuxRpm: `${releasePath}/NoteGen-${version}-1.x86_64.rpm`,
+    androidApk: `${releasePath}/NoteGen_${version}_android-universal.apk`,
+  } as const;
+}
+
+export type DownloadUrls = ReturnType<typeof getDownloadUrls>;
+export type DownloadUrlKey = keyof DownloadUrls;
